8849a75026f7e6d99e4e292db78313e4eb6a80f1
SocratesGuedes <socratesmascarenhas5gmail.com>
2024-01-07 16:51:39 -0600

    feat: add responsive

--- js/main.js
+++ js/main.js
@@ -11,3 +11,15 @@ var swiper = new Swiper(".mySwiper", {
     clickable: true,
   },
 });
+
+// Menu Open Close
+
+let menu = document.querySelector(".menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.addEventListener('click', () => {
+  menu.classList.toggle("move");
+  navbar.classList.toggle("open-menu")
+});
+
+// Close Menu On Scroll 
