748a7c0f392303be2842a75bef6b266c4edfef5c
SocratesGuedes <socratesmascarenhas5gmail.com>
2024-01-08 13:14:12 -0600

    feat: add animation

--- js/main.js
+++ js/main.js
@@ -23,3 +23,14 @@ menu.addEventListener('click', () => {
 });
 
 // Close Menu On Scroll 
+const animate = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: '2000',
+  delay: '100',
+});
+
+animate.reveal(".nav");
+animate.reveal(".home-text", {origin: 'left'});
+animate.reveal(".home-img", {origin: 'bottom'});
+animate.reveal(".ser-box, .product-box, .team-box, .book-data", {intervel: 100});
